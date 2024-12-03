cc764c8cc87e0df86776b54b8bf8bdb056d11204
SkydenQQQ <125984501+SkydenQQQ@users.noreply.github.com>
2024-12-04 00:13:37 +0800

    添加发送博客

---
+++ frontend/src/components/ProfilePage/MyBlogs.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "MyBlog"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/ProfilePage/PublishBlog.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "PostBlog"
+}
+</script>
+
+<style scoped>
+
+</style>
