fe54b8570a5f54e1a95330b19ec67a0afa014363
SkydenQQQ <125984501+SkydenQQQ@users.noreply.github.com>
2024-12-04 00:15:08 +0800

    添加vditor依赖

--- frontend/src/components/ProfilePage/PublishBlog.vue
+++ frontend/src/components/ProfilePage/PublishBlog.vue
@@ -1,13 +1,34 @@
 <template>
-
+<div id="vditor"></div>
 </template>
 
 <script>
+import Vditor from 'vditor';
+import "vditor/dist/index.css";
+
 export default {
-  name: "PostBlog"
+  name: "PostBlog",
+
+  mounted() {
+    this.contentEditor = new Vditor('vditor',{
+      toolbarConfig: {
+        pin: true
+      },
+      cache: {
+        enable: false,
+      },
+      height: 500,
+      preview: {
+        mode: 'both',
+      }
+    })
+  }
+
 }
 </script>
 
 <style scoped>
-
+#vditor{
+  text-align: left;
+}
 </style>
--- frontend/src/views/ProfilePage.vue
+++ frontend/src/views/ProfilePage.vue
@@ -1,64 +1,110 @@
 <template>
-  <div id="profile">
-    <!-- 头部信息，分为左右两个容器 -->
-    <div class="profile-section">
+  <div class="container">
+    <Topbar/>
+    <div id="profile">
+      <!-- 头部信息，分为左右两个容器 -->
       <!-- 左侧容器：头像和基本信息 -->
       <div class="left-container">
-        <div class="avatar">头像</div>
-        <div class="profile-info">
+        <div>
+          <div class="avatar">头像</div>
+        </div>
+        <div class="profile-info info-list-row">
           <p>用户名：Aberdeen Student</p>
           <p>简介：我是一个喜欢学习的软件工程师</p>
           <p>安全信息</p>
         </div>
       </div>
       <!-- 右侧容器：统计数据 -->
-      <div class="right-container">
+      <div class="right-container info-list-row">
         <p>发布博客量：10</p>
         <p>获赞量：100</p>
         <p>对战次数：1</p>
       </div>
-    </div>
-
-    <!-- 中间选项卡 -->
-    <div class="tabs-container">
-      <div class="tabs">
-        <button @click="setTab('myBlogs')">我的博客</button>
-        <button @click="setTab('publishBlog')">发布博客</button>
-        <button @click="setTab('likeRecords')">点赞记录</button>
-        <button @click="setTab('favorites')">收藏记录</button>
+      <div class="body-container">
+        <!-- 中间选项卡 -->
+        <div class="tabs-container">
+          <div class="tabs">
+            <button type="button" @click="setTab('myBlogs')" :class="activeTab==='myBlogs'?'activate-tab':''">我的博客</button>
+            <button type="button" @click="setTab('publishBlog')" :class="activeTab==='publishBlog'?'activate-tab':''">发布博客</button>
+            <button type="button" @click="setTab('likeRecords')" :class="activeTab==='likeRecords'?'activate-tab':''">点赞记录</button>
+            <button type="button" @click="setTab('favorites')" :class="activeTab==='favorites'?'activate-tab':''">收藏记录</button>
+          </div>
+        </div>
+        <!-- 内容展示 -->
+        <div class="content">
+<!--          <div v-if="activeTab === 'myBlogs'">这是我的博客内容</div>-->
+<!--          <div v-if="activeTab === 'publishBlog'">这是发布博客页面</div>-->
+<!--          <div v-if="activeTab === 'likeRecords'">这是点赞记录页面</div>-->
+<!--          <div v-if="activeTab === 'favorites'">这是收藏记录页面</div>-->
+          <component :is="selectComponents"/>
+        </div>
       </div>
-    </div>
-
-    <!-- 内容展示 -->
-    <div class="content">
-      <div v-if="activeTab === 'myBlogs'">这是我的博客内容</div>
-      <div v-if="activeTab === 'publishBlog'">这是发布博客页面</div>
-      <div v-if="activeTab === 'likeRecords'">这是点赞记录页面</div>
-      <div v-if="activeTab === 'favorites'">这是收藏记录页面</div>
     </div>
   </div>
 </template>
 
 <script>
+import Topbar from "@/components/Topbar.vue";
+import PublishBlog from "@/components/ProfilePage/PublishBlog.vue";
+import MyBlogs from "@/components/ProfilePage/MyBlogs.vue";
+import LikeRecords from "@/components/ProfilePage/LikeRecords.vue";
+import Favorites from "@/components/ProfilePage/Favorites.vue";
+
 export default {
+  components: {Topbar,PublishBlog,MyBlogs,LikeRecords,Favorites},
   data() {
     return {
       activeTab: "myBlogs", // 默认显示我的博客
+      selectComponents: MyBlogs
     };
   },
   methods: {
     setTab(tabName) {
       this.activeTab = tabName;
+      if (this.activeTab==='myBlogs'){
+        this.selectComponents = MyBlogs
+      }else if(this.activeTab==='publishBlog') {
+        this.selectComponents = PublishBlog
+      }else if(this.activeTab==='likeRecords') {
+        this.selectComponents = LikeRecords
+      }else if (this.activeTab==='favorites') {
+        this.selectComponents = Favorites
+      }
     },
   },
 };
 </script>
 
 <style scoped>
+.container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: calc(100vh - 16px);
+  gap: 20px;
+}
+
+.nav{
+  height: auto;
+}
+
 /* 通用样式 */
-#app {
+#profile {
   font-family: Arial, sans-serif;
   text-align: center;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 0.5fr 1fr;
+  grid-template-areas:
+    "a b"
+    "c c";
+  grid-gap: 20px;
+
+  height: 100%;
+  max-width: 1980px;
+  min-width: 1280px;
+
 }
 
 /* 头部信息容器样式 */
@@ -70,22 +116,36 @@ export default {
   margin: 20px;
 }
 
+.info-list-row{
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+}
+
 .left-container{
-  width: 45%;
+  grid-area: a;
   background-color: rgba(245, 139, 40, 0.8);
   border: 2px solid #e07821;
   border-radius: 10px;
   padding: 20px;
   color: white;
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
 }
 
 .right-container {
-  width: 45%;
+  grid-area: b;
   background-color: rgba(245, 139, 40, 0.8);
   border: 2px solid #e07821;
   border-radius: 10px;
   padding: 20px;
   color: white;
+  text-align: left;
+}
+
+.body-container{
+  grid-area: c;
 }
 
 .avatar {
@@ -103,17 +163,31 @@ export default {
 }
 
 /* 选项卡容器样式 */
+.body-container{
+  background-color: rgba(245, 139, 40, 0.8);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+}
+
 .tabs-container {
   width: 100%;
+  height: 10%;
+  min-height: 60px;
   background-color: rgba(245, 139, 40, 0.8);
-  padding: 10px 0;
-  margin-top: 20px;
+  padding: 0 0;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
 }
 
 .tabs {
-  display: flex;
-  justify-content: center;
-  gap: 20px;
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+
+  padding: 0 0;
 }
 
 .tabs button {
@@ -125,12 +199,19 @@ export default {
   cursor: pointer;
 }
 
+
 .tabs button:hover {
-  background-color: #e07821;
+  background-color: rgba(206, 110, 31, 0.75);
 }
+
+.activate-tab{
+  background-color: #e07821 !important;
+}
+
 
 /* 内容容器样式 */
 .content {
+  height: 80%;
   background-color: rgba(245, 139, 40, 0.8);
   color: white;
   padding: 20px;
