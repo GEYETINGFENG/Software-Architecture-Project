f84fe779e88090b45b32f4d72a7a7eba492bad69
SkydenQQQ <125984501+SkydenQQQ@users.noreply.github.com>
2024-12-07 15:44:30 +0800

    background and middle the component

--- frontend/src/views/Quiz.vue
+++ frontend/src/views/Quiz.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="app-container">
+    <video autoplay muted loop class="video-background">
+      <source src="../assets/img/Battel%20background.mp4" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
     <!-- 引入Topbar组件 -->
     <Topbar />
-
     <!-- 引入QuizContent组件 -->
-    <QuizContent />
+    <div class="middle-container">
+      <QuizContent />
+    </div>
   </div>
 </template>
 
@@ -28,5 +33,26 @@ export default {
   display: flex;
   flex-direction: column; /* 纵向布局，Topbar在上，QuizContent在下 */
   min-height: 100vh; /* 让容器的最小高度为视口高度，确保布局填满页面 */
+}
+
+.video-background {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+  width: auto;
+  height: auto;
+  opacity: 0.5;
+  z-index: -100;
+  background-size: cover;
+  overflow: hidden;
+}
+
+.middle-container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
